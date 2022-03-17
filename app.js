2bfabe00adc999e0ae4d93ceb3fc1b0eb0ce29ae
destroyer342 <ranceofficial@gmail.com>
2022-03-18 06:21:00 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
-console.log('rances')
+// ID : 954134777060016178
+// TOKEN : OTU0MTM0Nzc3MDYwMDE2MTc4.YjOtQQ.6oKbZTI1iAldpjFMBjkbs1zk4y0
+// Invite_link: https://discord.com/oauth2/authorize?client_id=954134777060016178&scope=bot&permissions=1
