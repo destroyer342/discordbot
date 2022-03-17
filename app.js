92e956871790fff05cf5b7b5c8ce977c1b3bfc5c
destroyer342 <ranceofficial@gmail.com>
2022-03-18 07:12:31 +0800

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 // ID : 954134777060016178
-
 // Invite_link: https://discord.com/oauth2/authorize?client_id=954134777060016178&scope=bot&permissions=1
 require("dotenv").config();
 const discord = require('discord.js')
@@ -7,4 +6,18 @@ const client = new discord.Client({
     intents:["GUILDS","GUILD_MESSAGES", "DIRECT_MESSAGES"], partials:["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION", "USER"]
 });
 
+
+
+client.on("ready", (clientparams) => {
+    console.log("this bot is now online " +client.user.tag)
+})
+
+client.on("messageCreate", (message) => {
+if (message.author.bot == false){
+
+    message.reply(`${message}`)
+   
+    
+}
+})
 client.login(process.env.TOKEN)
